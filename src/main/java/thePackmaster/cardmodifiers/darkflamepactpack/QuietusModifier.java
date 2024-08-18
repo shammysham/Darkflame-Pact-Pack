6e09e55a0cd50388d43fc59b32c00b79539f41c0
shammysham <139022155+shammysham@users.noreply.github.com>
2024-08-17 20:20:06 -0400

    quietus now seems to work across the board

--- src/main/java/thePackmaster/cardmodifiers/darkflamepactpack/QuietusModifier.java
+++ src/main/java/thePackmaster/cardmodifiers/darkflamepactpack/QuietusModifier.java
@@ -2,15 +2,20 @@ package thePackmaster.cardmodifiers.darkflamepactpack;
 
 import basemod.abstracts.AbstractCardModifier;
 import basemod.helpers.CardModifierManager;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.utility.NewQueueCardAction;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.UIStrings;
+import com.megacrit.cardcrawl.random.Random;
 import thePackmaster.SpireAnniversary5Mod;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class QuietusModifier extends AbstractCardModifier {
@@ -48,13 +53,17 @@ public class QuietusModifier extends AbstractCardModifier {
   }
 
   public static Optional<QuietusModifier> getFrom(AbstractCard c) {
-    return CardModifierManager.getModifiers(c, QuietusModifier.ID).stream().map(cm -> (QuietusModifier) cm).findFirst();
+    return c == null ? Optional.empty() : CardModifierManager.getModifiers(c, QuietusModifier.ID).stream().map(cm -> (QuietusModifier) cm).findFirst();
   }
 
   @Override
   public void onUse(AbstractCard card, AbstractCreature target, UseCardAction action) {
     // reset trigger
     this.isBeingPlayedByQuietusTrigger = false;
+  }
+
+  public boolean isBeingPlayedByQuietusTrigger() {
+    return isBeingPlayedByQuietusTrigger;
   }
 
   @Override
@@ -67,17 +76,49 @@ public class QuietusModifier extends AbstractCardModifier {
   }
 
   public static void process() {
+    List<AbstractCard> toRemove = new ArrayList<>();
     AbstractDungeon.player.hand.group.stream()
         .filter(c -> {
           Optional<QuietusModifier> q = getFrom(c);
           return q.isPresent() && !q.get().isBeingPlayedByQuietusTrigger;
         })
         .forEach(c -> {
-          // we will use this to allow the card to be played outside the player's turn, as well as allow
-          // unplayables to be played
-          getFrom(c).ifPresent(q -> q.isBeingPlayedByQuietusTrigger = true);
-          AbstractDungeon.actionManager.addToTop(new NewQueueCardAction(c, true, false, true));
+          processSingleCard(c);
+          toRemove.add(c);
         });
+
+    AbstractDungeon.player.limbo.group.addAll(toRemove);
+    AbstractDungeon.player.hand.group.removeAll(toRemove);
+
+    AbstractDungeon.player.limbo.group.stream()
+        .filter(c -> {
+          Optional<QuietusModifier> q = getFrom(c);
+          return q.isPresent() && (c.retain || c.selfRetain);
+        })
+        .forEach(c -> {
+          AbstractDungeon.actionManager.addToBottom(new AbstractGameAction() {
+            @Override
+            public void update() {
+              this.isDone = true;
+              AbstractDungeon.player.limbo.group.add(c);
+              AbstractDungeon.player.hand.group.remove(c);
+            }
+          });
+          processSingleCard(c);
+        });
+  }
+
+  private static void processSingleCard(AbstractCard c) {
+    Random random = new Random();
+    // we will use this to allow the card to be played outside the player's turn, as well as allow
+    // unplayables to be played
+    getFrom(c).ifPresent(q -> q.isBeingPlayedByQuietusTrigger = true);
+    c.target_y = Settings.HEIGHT / 2.0f + random.random(-100.0f, 300.0f);
+    c.target_x = Settings.WIDTH / 2.0f + random.random(-Settings.WIDTH / 4.0f, Settings.WIDTH / 4.0f);
+    c.targetAngle = 0;
+    c.targetDrawScale = 0.8f;
+    c.lighten(true);
+    AbstractDungeon.actionManager.addToTop(new NewQueueCardAction(c, true, false, true));
   }
 
   public interface UnplayableQuietusCard {
--- src/main/java/thePackmaster/patches/darkflamepactpack/DarkflamePactPatches.java
+++ src/main/java/thePackmaster/patches/darkflamepactpack/DarkflamePactPatches.java
@@ -1,9 +1,21 @@
 package thePackmaster.patches.darkflamepactpack;
 
-import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.megacrit.cardcrawl.actions.GameActionManager;
+import com.megacrit.cardcrawl.actions.common.EndTurnAction;
+import com.megacrit.cardcrawl.actions.common.MonsterStartTurnAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.actions.utility.WaitAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardQueueItem;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
+import com.megacrit.cardcrawl.vfx.EnemyTurnEffect;
+import javassist.CtBehavior;
+import thePackmaster.cardmodifiers.darkflamepactpack.QuietusModifier;
 import thePackmaster.packs.DarkflamePactPack;
+
+import java.util.Optional;
 
 @SuppressWarnings("unused")
 public class DarkflamePactPatches {
@@ -16,6 +28,96 @@ public class DarkflamePactPatches {
     @SpireInsertPatch(rloc = 3)
     public static void doQuietusCheck() {
       DarkflamePactPack.triggerOnDiscardOrExhaust();
+    }
+  }
+
+  @SpirePatch(
+      clz = AbstractCard.class,
+      method = "hasEnoughEnergy"
+  )
+  public static class PlayOutOfTurnPatch {
+    @SpirePrefixPatch
+    public static SpireReturn<Boolean> bypassCheckForQuietus(AbstractCard __instance) {
+      Optional<QuietusModifier> q = QuietusModifier.getFrom(__instance);
+      if(q.isPresent() && q.get().isBeingPlayedByQuietusTrigger()) {
+        return SpireReturn.Return(true);
+      }
+      return SpireReturn.Continue();
+    }
+  }
+
+  @SpirePatch(
+      clz = UseCardAction.class,
+      method = "update"
+  )
+  public static class UseCardActionInsertPatch {
+    @SpireInsertPatch(
+        locator = Locator.class
+    )
+    public static void Insert(UseCardAction __instance, AbstractCard ___targetCard) {
+      AbstractDungeon.player.limbo.removeCard(___targetCard);
+
+      if(!AbstractDungeon.actionManager.turnHasEnded) {
+        return;
+      }
+
+      for (CardQueueItem c : AbstractDungeon.actionManager.cardQueue) {
+        if (QuietusModifier.getFrom(c.card).isPresent() && c.card != ___targetCard) {
+          return;
+        }
+      }
+      AbstractDungeon.topLevelEffects.add(new EnemyTurnEffect());
+    }
+
+    private static class Locator extends SpireInsertLocator {
+      @Override
+      public int[] Locate(CtBehavior ctMethodToPatch) throws Exception {
+        return LineFinder.findInOrder(ctMethodToPatch, new Matcher.FieldAccessMatcher(AbstractCard.class, "freeToPlayOnce"));
+      }
+    }
+  }
+
+  @SpirePatch(
+      clz = EndTurnAction.class,
+      method = "update"
+  )
+  public static class StopEnemyTurnEffect {
+    @SpireInsertPatch(
+        locator = Locator.class
+    )
+    public static SpireReturn<Void> Insert(EndTurnAction __instance) {
+      __instance.isDone = true;
+      for(CardQueueItem cqi : AbstractDungeon.actionManager.cardQueue) {
+        if(QuietusModifier.getFrom(cqi.card).isPresent()) {
+          return SpireReturn.Return();
+        }
+      }
+      return SpireReturn.Continue();
+    }
+
+    private static class Locator extends SpireInsertLocator {
+      @Override
+      public int[] Locate(CtBehavior ctMethodToPatch) throws Exception {
+        Matcher finalMatcher = new Matcher.FieldAccessMatcher(AbstractRoom.class, "skipMonsterTurn");
+        return LineFinder.findInOrder(ctMethodToPatch, finalMatcher);
+      }
+    }
+  }
+
+  @SpirePatch(
+      clz = MonsterStartTurnAction.class,
+      method = SpirePatch.CONSTRUCTOR
+  )
+  public static class DontDelay {
+    public static void Postfix(MonsterStartTurnAction __instance) {
+      for (CardQueueItem cqi : AbstractDungeon.actionManager.cardQueue) {
+        if (!QuietusModifier.getFrom(cqi.card).isPresent()) {
+          continue;
+        }
+
+        AbstractDungeon.actionManager.actions.removeIf(action -> action instanceof WaitAction);
+        return;
+      }
     }
   }
 }
